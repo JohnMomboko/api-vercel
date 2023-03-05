f13ea1c2e7935fa90ad8b7d8f2bdc9071ba4606e
JohnMomboko <vevemomboko@gmail.com>
2023-03-05 13:10:18 +0100

    message

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const cors = require("cors")
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/veve",(req, res, next) => {
+    res.json({message: 'Je suis John Momboko'})
+    next()
+
+})
+
+app.use((req, res) => {
+    res.json({message: 'Je suis une api'})
+})
